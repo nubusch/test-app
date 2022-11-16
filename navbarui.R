8a8113bcad64f3f26ce47a1623c4b0f618e00f61
nubusch <t.y.j.boezewinkel@gmail.com>
2022-11-16 19:42:21 +0100

    sure

--- navbarui.R
+++ navbarui.R
@@ -5,16 +5,7 @@ library(dplyr)
 library(bslib)
 library(sendmailR)
 library(data.table)
-
-
-
-
-
-
-
-
-
-
+library(fastDummies)
 
 
 
@@ -36,12 +27,12 @@ ui <- navbarPage("Nubs advice for movies and serie", fluid = TRUE, theme = bs_th
                                                              choices = sort(c("Animation", "Action", "Adventure", "Comedy", "Crime", "Drama", "Family", "Fantasy", "Romance", "Mystery", "Scifi",  "Music", "Thriller", "Musical", "Horror", "History", "War", "Biography", "Sport", "Short", "Documentary", "Western"  ))),
                                    
                                    
-                                   column(6,
+                                   column(3, offset = 2,
                                           
                                           
                                           
                                           
-                                          mainPanel(dataTableOutput("choice"))
+                                          mainPanel(dataTableOutput("table"))
                                           
                                           
                                           
@@ -127,6 +118,54 @@ ui <- navbarPage("Nubs advice for movies and serie", fluid = TRUE, theme = bs_th
 
 server <- function(input, output) {
   
+  
+  id <- "15mfq838c4f9X-WC8n8mwXBtugOlzeVpf" 
+  films <- read.csv(paste0("https://docs.google.com/uc?id=",id,"&export=download"))
+  
+  films <- films[-c(1,26)]
+  films <- films[-c(1:3),]
+  
+  
+  names(films) <- c( "name", "FilmorSerie", "Comedy", "Drama", "Family", "Fantasy", "Action", "Crime","Western","Romance","Adventure","Mystery",
+                     "Scifi","Animation","Music","Thriller","Musical","Horror","History","War","Biography","Sport","Short","Documentary")
+  
+  
+  output$table <- renderDataTable({
+    data.table(films[, input$genre, drop = FALSE])
+  })
+  
+  
+  
+  
+  
+  Animation1 <- filter(films, Animation == "x")
+  Action <- filter(films, Action == "x")
+  Adventure <- filter(films, Adventure == "x")
+  Comedy <- filter(films, Comedy == "x")
+  Crime <- filter(films, Crime == "x")
+  Drama <- filter(films, Drama == "x")
+  Family <- filter(films, Family == "x")
+  Fantasy <- filter(films, Fantasy == "x")
+  Romance <- filter(films, Romance == "x")
+  Mystery <- filter(films, Mystery == "x")
+  Scifi <- filter(films, Scifi == "x")
+  Music <- filter(films,  Music == "x")
+  Thriller <- filter(films, Thriller == "x")
+  Musical <- filter(films, Musical == "x")
+  Horror <- filter(films, Horror == "x")
+  History <- filter(films, History == "x")
+  War <- filter(films,  War == "x")
+  Biography <- filter(films, Biography == "x")
+  Sport <- filter(films, Sport == "x")
+  Short <- filter(films, Short == "x")
+  Documentary <- filter(films, Documentary == "x")
+  Western <- filter(films, Western == "x")
+  
+  
+  
+  
+  
+
   
   
   
